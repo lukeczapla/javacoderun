67ef36cbf6a7e95f58fc2d0cd38dd00518abf17a
luke czapla <luke@lukes-MacBook-Pro.local>
2017-05-26 14:38:41 -0400

    Update

--- examples/test.java
+++
@@ -1,8 +0,0 @@
-import java.io.*;
-import java.util.*;
-class mytest {
-  public static void main(String[] args) throws Exception {
-    System.out.println("hello world");
-    PrintWriter out = new PrintWriter(new File("test.java22"));
-  }
-}
